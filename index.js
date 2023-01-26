1eb840381f185a77660afe30df45e8210286b0dd
Vaibhav Bhardwaj <110530263+vaibhavx42@users.noreply.github.com>
2023-01-26 20:37:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById('getDetail').addEventListener('click', getApi);
+function getApi() {
+    var AnimeName = document.getElementById('animeName').value;
+console.log(AnimeName)
+const url=`https://api.waifu.pics/sfw/${AnimeName}`;
+console.log(url);
+fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        let output = `
+        <img width="100%" height="50%" src="${data.url}" alt="" srcset="">`;
+        console.log('printing data', data);
+        document.getElementById('output').innerHTML = output;
+    }).catch((err) => console.log(err))
+}
